0bd98f4a4abe5cb790b31e1270315634f33667e1
2892211452 <54618163+2892211452@users.noreply.github.com>
2021-12-28 10:36:04 +0800

    第二次更新
    因为官网修改了接口，所以我们这也得改

--- main2.0.py
+++ main2.0.py
@@ -13,21 +13,73 @@ class csuLibrary():
         self.pwd = "294633"
         self.area = "53"
         self.segment = ''
-        self.seatListFun={}
 
     # 登录，获取一个验证id，这个id将会用于获取token
     def login(self):
 
-        # 登录，获取一个验证id，这个id将会用于获取token
 
-        checkUrl = 'http://libzw.csu.edu.cn/Api/auto_user_check?' + 'user=8209180334&p=b87ccdc40713b8d1c5b37634a7dad9c0&callback=http://libzw.csu.edu.cn/web/seat3?area=53'
+        data = {
+        'appId': 'cascsu',
+        "retUrl": "http://libzw.csu.edu.cn/sso/home.php",
+        "params":"",
+        "openId":"",
+        "userId": self.username,
+        "password":self.pwd
+        }
+
+
+        headers1 = getHeader(url = 'reqFile/request')
+
+        # for i in headers1:
+        #     session.headers[i] = headers1[i]
+
+        url = 'https://ca.csu.edu.cn/authserver/checkNeedCaptcha.htl?username=8209180334&_=1640607470116'
+
+        try:
+            resp = self.session.get(url = url,  headers = headers1,allow_redirects=True)
+        except Exception as e:
+            print('error occur',e)
+
+        # session.cookies['access_token'] = '58eca0ee5625dac2b965f2e378783f5e'
+        # print(self.session.cookies)
+
+
+
+    # 获取token
+    def getToken(self):
+
+        # 计算token接口页面
+        home = 'http://libzw.csu.edu.cn/sso/home.php'
+
+        headers2 = getHeader('reqFile/req2')
+
+        homereq = self.session.get(url=home, headers=headers2)
+
+
+        s = homereq.text
+        print(s)
+        import re
+        # for i in s:
+        #     print(i)
+        ans  = re.search('user=.*;', s)
+        ans = ans.group()
+        ans = ans.replace('";', '')
+        ans
+
+
+        # token接口页面
+        checkUrl = 'http://libzw.csu.edu.cn/Api/auto_user_check?' + ans
+        # print(checkUrl)
 
         reqToken = self.session.get(url=checkUrl)
 
+
+    # 获取时间戳，每次要用之前访问一下
     def getSegment(self):
         day, hour, minute = getNow()
 
-        url = 'http://libzw.csu.edu.cn/api.php/space_time_buckets?day='+day+'&area=87'
+        url = 'http://libzw.csu.edu.cn/api.php/space_time_buckets?day='+day+'&area=' + self.area
+
         seatHeader2 = getHeader('reqFile/reqSeat2')
 
         spaceReq = self.session.get(url=url, headers=seatHeader2)
@@ -40,14 +92,12 @@ class csuLibrary():
 
 
 
-
-    # 目前固定了铁道2楼的url， 返回目前的空闲位置
     def checkRest(self):
         day, hour, minute = getNow()
         # 这里是参数，固定了铁道2楼就是这个
-        # self.getSegment()
-        # print(self.segment)
-        spaceUrl = 'http://libzw.csu.edu.cn/api.php/spaces_old?area=53&segment=1519031+'+'&day=' + day + '&startTime=' + str(hour) + ':' + str(minute) + '&endTime=22%3A00'
+        spaceUrl = 'http://libzw.csu.edu.cn/api.php/spaces_old?area='+self.area+'&segment='+self.segment+'&day=' + day + '&startTime=' + str(
+            hour) + ':' + str(minute) + '&endTime=22%3A00'
+
         seatHeader2 = getHeader('reqFile/reqSeat2')
 
         spaceReq = self.session.get(url=spaceUrl, headers=seatHeader2)
@@ -66,7 +116,7 @@ class csuLibrary():
             # if name == 'TF2A014':
             #     print(i['status_name'], i['name'], i, end='\n\n')
 
-        self.seatListFun = rest
+
         return rest
 
 
@@ -78,7 +128,7 @@ class csuLibrary():
         formData['access_token'] = self.session.cookies['access_token']
         formData['userid'] = self.session.cookies['userid']
 
-        formData['segment'] = '1519031'
+        formData['segment'] = self.segment   # 这里是参数，固定了铁道2楼就是这个
         formData['type'] = str(1)
         seatUrl  = 'http://libzw.csu.edu.cn/api.php/spaces/' +str(id)+ '/book'
         print(seatUrl)
@@ -97,30 +147,24 @@ class csuLibrary():
 
 if __name__ == '__main__':
     csu = csuLibrary()
-    csu.getSegment()
-    print(csu.segment)
-    if True:  # 是否需要获取新的token
+    flag = 'new'
+    if flag == 'new':  # 是否需要获取新的token
         csu.login()
         print(csu.session.cookies)
-        csu.saveCookies()
 
+        csu.getToken()
     else:
         csu.loadCookies()
 
     # 获取segment，也就是今天的时间标记, 必须的
+    csu.getSegment()
 
-#    获取空闲位子
+    # 获取空闲位子
     rest = csu.checkRest()
+
     print('空闲的位子有')
     for i in rest:
         print(i)
-
-
-    id = csu.seatListFun = rest['XF4E013']
-    choose = csu.chooseSeat(id)
-    print(choose)
-
-
 
     # print( csu.segment,csu.session.cookies)
     # ans = csu.chooseSeat(rest['TF2A014'])
